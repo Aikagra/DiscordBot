da6396873099e5651c519d275554cdc0bdec8e56
shells <aikagra2007@gmail.com>
2021-07-13 10:52:15 +0530

    first commit

---
+++ commands/kick.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'kick',
+    description: 'this command kicks a member!',
+    execute(message, args){
+      const member = message.mentions.users.first();
+      if(member){
+        const memberTarger = message.guild.members.cache.get(member.id);
+        memberTarger.kick();
+        message.channel.send("User has been kicked lol");
+      } else {
+          message.channel.send('You could not kick that member, noob');
+      }
+    }
+}
