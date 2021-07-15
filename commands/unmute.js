6fa8649d44f00f86fde5a9687e7eca98e5fa6d3f
shells <aikagra2007@gmail.com>
2021-07-15 12:34:23 +0530

    asdf

--- commands/unmute.js
+++ commands/unmute.js
@@ -4,8 +4,8 @@ module.exports = {
     execute(message, args) {
         const target = message.mentions.users.first();
         if (target) {
-            let mainRole = message.guild.roles.cache.find(role => role.name === 'mute');
-            let muteRole = message.guild.roles.cache.find(role => role.name === 'noob mods');
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'member');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'mute');
 
             let memberTarget = message.guild.members.cache.get(target.id);
 
