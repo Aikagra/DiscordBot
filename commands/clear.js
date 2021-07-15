b68ad890f30d46b19befed3162c72b41e9a21c16
shells <aikagra2007@gmail.com>
2021-07-15 12:33:15 +0530

    yes

--- commands/clear.js
+++ commands/clear.js
@@ -1,15 +1,15 @@
 module.exports = {
     name: 'clear',
     description: 'Clear Message',
-    async execute(message, args){
-       if(!args[0]) return message.reply('Specify how many messages you want to delete you noob');
-       if(isNaN(args[0])) return message.reply('Enter a frickin real number you noob');
+    async execute(message, args) {
+        if (!args[0]) return message.reply('Specify how many messages you want to delete you noob');
+        if (isNaN(args[0])) return message.reply('Enter a frickin real number you noob');
 
-       if(args[0] > 100) return message.reply('You cannot delete more than 100 you noob');
-       if(args[0] < 1) return message.reply('Delete atleast one message you noob!');
+        if (args[0] > 100) return message.reply('You cannot delete more than 100 you noob');
+        if (args[0] < 1) return message.reply('Delete atleast one message you noob!');
 
-       await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
-        message.channel.bulkDelete(messages);
-       });
+        await message.channel.messages.fetch({ limit: args[0] }).then(messages => {
+            message.channel.bulkDelete(messages);
+        });
     }
 }
--- commands/mute.js
+++ commands/mute.js
@@ -5,12 +5,12 @@ module.exports = {
     execute(message, args) {
         const target = message.mentions.users.first();
         if (target) {
- 
-            let mainRole = message.guild.roles.cache.find(role => role.name === 'noob mods');
+
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'member');
             let muteRole = message.guild.roles.cache.find(role => role.name === 'mute');
- 
+
             let memberTarget = message.guild.members.cache.get(target.id);
- 
+
             if (!args[1]) {
                 memberTarget.roles.remove(mainRole.id);
                 memberTarget.roles.add(muteRole.id);
@@ -20,7 +20,7 @@ module.exports = {
             memberTarget.roles.remove(mainRole.id);
             memberTarget.roles.add(muteRole.id);
             message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`);
- 
+
             setTimeout(function () {
                 memberTarget.roles.remove(muteRole.id);
                 memberTarget.roles.add(mainRole.id);
